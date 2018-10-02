54f674f629bd81ee0519bbfe0706d721b8759750
xipeRafa <rafaelsistem87@gmail.com>
2018-10-01 18:53:52 -0700

    A-S

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,9 @@
 class Pin < ApplicationRecord
   acts_as_votable
     belongs_to :user
-
+    
+    has_one_attached :image
+    
     has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
